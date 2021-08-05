b08fb1d7f2f9f0e95788bb1b2e5de6c2147de79c
PlaneCargo191 <t.james_17@hotmail.com>
2021-08-05 14:16:31 +0700

    Author Section DONE

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
-// define more constants and variables here
+const search = document.querySelector("#search")
+const text = document.querySelector("#text")
+let tg = 0
 
 btn_toggle.onclick = () => {
-  // your code here
+  if(tg === 0) {
+    const cal = 620610790 - length.value
+    author.innerText = cal
+    btn_toggle.innerText = "Show Author"
+    tg = 1
+  } else {
+    author.innerText = "620610790 Thanakorn"
+    btn_toggle.innerText = "Show Calculation"
+    tg = 0
+  }
 }
 
-// more codes for Search and Reset buttons here
